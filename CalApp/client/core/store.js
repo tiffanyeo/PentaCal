db44ed18cda216291da6f69a2f1f2181ccfbf9ec
Neo <neogoldahlgren@gmail.com>
2026-03-05 14:55:29 +0100

    Update store.js
    bla

--- CalApp/client/core/store.js
+++ CalApp/client/core/store.js
@@ -1,19 +1,22 @@
 class Store {
     static allStates = [];
+    static listeners = {};
     constructor(initialState) {
         this.state = initialState;
-        Store.allStates.push(this.state);
-        this.listeners = []; 
+        this.lastState = null;
+        Store.allStates.push(this.state)
     }
     getState() {
         return structuredClone(this.state);
     }
     setState(newState) {
+        this.lastState = this.state;
         this.state = newState;
         this.notify();
     }
-    subscribe(listener) {
-        this.listeners.push(listener); 
+    subscribe(eventName, listener) {
+        if (!Store.listeners[eventName]) Store.listeners[eventName] = [];
+        Store.listeners[eventName].push(listener);
     }
     notify() {
         this.listeners.forEach(listener => listener());
