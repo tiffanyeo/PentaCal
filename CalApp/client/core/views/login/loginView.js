94b7ad7e5218508398f6b7e47c463c61c19d87e0
Tiffany <tiffanylarsson.stud@gmail.com>
2026-03-11 10:41:28 +0100

    Update loginView.js

--- CalApp/client/core/views/login/loginView.js
+++ CalApp/client/core/views/login/loginView.js
@@ -14,7 +14,29 @@ export class HomeView extends HTMLElement{
     }
     render(){
 
-        this.shadowRoot.innerHTML = ``;
+        this.shadowRoot.innerHTML = `
+            <link rel="stylesheet" href="/CalApp/client/core/views/login/loginView.css">
+            
+            <h2>Welcome</h2>
+            
+            <app-input
+                label="Description"
+                placeholder="Enter username"
+                width="100%"
+                height="100px"
+                id="username"
+            ></app-input>
+            
+            <app-input
+                label="Description"
+                placeholder="Enter password"
+                width="100%"
+                height="100px"
+                id="password"
+            ></app-input>
+            
+            <button id="Log in">Login</button>
+        `;
     }
 
 }
