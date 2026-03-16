4c3549cfcf18d17bf856bc2ac48d7ef13a61017a
Tiffany <tiffanylarsson.stud@gmail.com>
2026-03-16 08:57:18 +0100

    Create userGroupsService.js

---
+++ CalApp/client/core/services/userGroupsService.js
@@ -0,0 +1,39 @@
+import { apiRequest } from "./api.js";
+import { PubSub } from "../store/pubsub.js";
+import { Store } from "../store/store.js";
+import { EVENTS } from "../store/events.js";
+
+export function userGroupsService() {
+
+    PubSub.subscribe(EVENTS.REQUEST.SENT.USERGROUPS.POST, async payload => {
+
+        try {
+            
+            const response = await apiRequest({
+                entity: "usergroups",
+                method: "POST",
+                body: payload
+            });
+
+            PubSub.publish(EVENTS.RESPONSE.RECEIVED.USERGROUPS.POST, response);
+            PubSub.publish(EVENTS.RESOURCE.RECEIVED.USERGROUPS.POST, response);
+
+            // Uppdatera store om OK
+            const curr = Store.getState().data.usergroups || [];
+            const updatedUG = [...curr, response];
+
+            Store.setState({
+                data: {
+                    ...Store.getState().data,
+                    usergroups: updatedUG
+                }
+            });
+
+        } catch (err) {
+            
+            console.error("USERGROUPS POST ERROR:", err);
+            PubSub.publish(EVENTS.RESPONSE.ERROR.USERGROUPS.POST, err);
+            
+        }
+    });
+}
