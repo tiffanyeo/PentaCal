70ea95be155372399f30572dcdc0cb6c0b060f73
Norrheden <eliasnorrhedenmau@gmail.com>
2026-03-16 10:55:02 +0100

    .

--- CalApp/client/core/router/router.js
+++ CalApp/client/core/router/router.js
@@ -1,94 +1,29 @@
 import { store } from "../store/store.js";
+//import { PubSub } from "../store/pubsub.js";
 
-// Andra förslag på lösning av router? 
-
-function resolveRoute(path) {
+export function routerHandle(path) {
     const cleanPath = path.split("?")[0];
+    console.log(cleanPath)
+    switch (cleanPath) {
+        case "/":
+            console.log("currentPage: home")
+            store.setState({ currentPage: "home" });
+            break;
 
-    let view;
-    // Dynamisk route: /events/event/3 -> kom på bättre lösning
-    if (cleanPath.startsWith("/events/event/")) {
-        const id = cleanPath.split("/").pop();
-        store.setState({
-            currentPage: "eventDetails",
-            params: { id }
-        });
-        store.notify("pageChanged");
-        return;
-    }
+        case "/calendar":
+            console.log("currentPage: calendar")
+            store.setState({ currentPage: "calendar" });
+            break;
 
-    // Gör lösning ovan så detta fungerar
-    view = path[1];
-    store.setState({ currentPage: view });
-
-
-    /*     switch (cleanPath) {
-            case "/":
-                store.setState({ currentPage: "home" });
-                break;
-    
-            case "/calendar":
-                store.setState({ currentPage: "calendar" });
-                break;
-    
-            default:
-                store.setState({ currentPage: "notfound" });
-                break;
-        } */
-
-
+        default:
+            console.log("currentPage: notFound")
+            store.setState({ currentPage: "notfound" });
+            break;
+    } 
 }
 
-export const Router = {
-
-    navigate(path) {
-        history.pushState({}, "", path);
-        resolveRoute(path);
-    },
-
-    init() {
-        resolveRoute(window.location.pathname);
-
-        window.addEventListener("popstate", () => {
-            resolveRoute(window.location.pathname);
-        });
-    }
-};
 
 
 
 
 
-
-// ROUTER FÖRSLAG
-// ROUTERN PUBLICERAR ETT EVENT, MED URL OCH VYN SUBSCRIBAR PÅ URL ELLER PARAMS OCH RENDERAR
-import { PubSub } from "../store/pubsub.js";
-
-export class TestRouter {
-    constructor(url) {
-        this.url = url.split("/").filter(Boolean);
-        this.mainPath = this.url[0];
-        this.subPath = this.url[1];
-
-        PubSub.publish("change:view", {
-            url: this.url,
-            mainPath: this.mainPath,
-            subPath: this.subPath,
-        })
-        console.log("router fired", this.mainPath, this.subPath);
-
-    }
-
-    navigate(path) {
-        history.pushState({}, "", path);
-        new TestRouter(path);
-    }
-
-    init() {
-        window.addEventListener("popstate", () => {
-            new TestRouter(window.location.pathname);
-        });
-    }
-
-
-}
--- CalApp/client/index.js
+++ CalApp/client/index.js
@@ -1,28 +1,25 @@
 // ROUTER
-import { Router } from "./core/router/router.js"
-import { TestRouter } from "./core/router/router.js";
+import { routerHandle } from "./core/router/router.js"
 
 // GLOBAL COMPONENTS
+/*
 import "./components/appInput/appInput.js";
 import "./components/bottomNav/bottomNav.js";
 import "./components/toggleBtn/toggleBtn.js";
+*/
+
+
 
 
 // SERVICES. (views i renderApp.js)
+/*
 import "../services/calendarService.js";
 import "./core/services/calendarService.js";
 
+*/
+
 console.log("index loaded")
 
-Router.init();
+routerHandle(window.location.pathname);
 
-
-
-// TEST
-
-import "./core/views/groupLanding/groupLanding.js";
-
-let test = new TestRouter(window.location.pathname);
-
-test.init();
 
