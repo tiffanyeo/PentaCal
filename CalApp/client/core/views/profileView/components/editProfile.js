119c9ae8d638f5c9c4b8f5ae828a0dfbc216f537
Norrheden <eliasnorrhedenmau@gmail.com>
2026-03-19 16:06:52 +0100

    .

--- CalApp/client/core/views/profileView/components/editProfile.js
+++ CalApp/client/core/views/profileView/components/editProfile.js
@@ -1,3 +1,4 @@
+import {apiRequest} from "../../../services/api.js"
 
 export default class EditProfile extends HTMLElement {
     constructor() {
@@ -10,16 +11,63 @@ export default class EditProfile extends HTMLElement {
                     display:flex;
                     flex-direction: column;
                 }
+                #container div {
+                    margin-top: 7px;
+                    background-color: #999898;
+                    padding: 8px;
+                    border-radius: 10px;
+                }
                 
             </style>
             <div id="container">
-                <input placeholder="New username"></input>
-                <input placeholder="Change password"></input>
-                <input placeholder="Repeat password"></input>
-                <button>Submit</button>
+                <div id="usernameDiv">
+                    <app-input placeholder="New username"></app-input>
+                    <regular-button>Save changes</regular-button>
+                </div>
+                <div id="passDiv">
+                    <app-input placeholder="New password"></app-input>
+                    <app-input placeholder="Reapet password"></app-input>
+                    <regular-button>Save changes</regular-button>
+
+                </div>
+                <div id="emailDiv">
+                    <app-input placeholder="New email"></app-input>
+                    <regular-button>Save changes</regular-button>
+                </div>
+                
             </div>
-            
         `
+        this.logic();
+    }
+    logic() {
+        const usernameDiv = this.shadowRoot.getElementById("usernameDiv");
+        const passDiv = this.shadowRoot.getElementById("passDiv");
+        const emailDiv = this.shadowRoot.getElementById("emailDiv");
+
+        const usernameBtn = usernameDiv.querySelector("regular-button");
+        const passBtn = passDiv.querySelector("regular-button");
+        const emailBtn = emailDiv.querySelector("regular-button");
+
+        usernameBtn.addEventListener("click", () => { //vrf arraow istället för function()
+            const usernameInput = usernameDiv.querySelector("app-input");
+            this.updateUsername(usernameInput.value);
+        })
+        passBtn.addEventListener("click", () => {
+            this.updatePass();
+        })
+        emailBtn.addEventListener("click", () => {
+            this.updateEmail();
+        })
+    
+    }
+    updateUsername() {
+        
+    }
+    updatePass() {
+
+    }
+    updateEmail() {
+
     }
 }
 customElements.define("profile-edit-comp", EditProfile);
